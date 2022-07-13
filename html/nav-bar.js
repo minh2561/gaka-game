64209535be62913aef79470809b34630d5e28ce9
unknown <minhminh250601@gmail.com>
2022-07-13 22:21:18 +0700

    first commit

---
+++ html/nav-bar.js
@@ -0,0 +1,34 @@
+document.write(`
+
+<ul class="navbar-nav me-auto mb-2 mb-lg-0 style-font">
+<li class="nav-item">
+  <a class="nav-link active navbar-icon" aria-current="page" href="#"><img src="./213/Newfolder/Rectangle4821.png" width="80px" alt=""></a>
+</li>
+<li class="nav-item dropdown nav-bar-icon">
+  <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <img src="/213/Newfolder/Frame45262(0).png" alt="" width="30px">
+    GAME
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <li><a class="dropdown-item" href="https://lienminh.garena.vn/">League of Legends</a></li>
+    <li><a class="dropdown-item" href="https://fo4.garena.vn/">FIFA Online 4</a></li>
+    <li><a class="dropdown-item" href="https://www.ign.com/games/the-last-of-us-2">The Last of Us Part II</a></li>
+  </ul>
+</li>
+<li class="nav-item nav-bar-icon">
+  <a class="nav-link" href="#"><img src="/213/Newfolder/Frame45262.png" alt="" width="30px"> GIFTCODE</a>
+</li>
+<li class="nav-item nav-bar-icon">
+  <a class="nav-link" href="#"><img src="/213/Newfolder/Frame45261.png" alt="" width="30px"> GIẢI ĐẤU</a>
+</li>
+<li class="nav-item nav-bar-icon">
+  <a class="nav-link" href="#"><img src="/213/Newfolder/Frame45261(1).png" alt="" width="30px"> ĐẶC QUYỀN VIP</a>
+</li>
+<li class="nav-item nav-bar-icon">
+  <a class="nav-link" href="#"><img src="/213/Newfolder/Frame45262(2).png" alt="" width="30px"> NẠP GẠO</a>
+</li>
+</ul>
+
+
+`);
+
---
+++ html/search.js
@@ -0,0 +1,8 @@
+document.write(`
+<form class="d-flex div-search">
+  <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+  <input class="form-control me-2 input-search" type="search" placeholder="Tìm kiếm game..." aria-label="Search">
+</form>
+<button class="btn btn-outline-danger btn-signIn div-search" type="submit">Đăng nhập</button>
+
+`);
