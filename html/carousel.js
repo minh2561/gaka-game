55b974015f140af0bf2e135d52e983106967e927
minh2561 <84631891+minh2561@users.noreply.github.com>
2022-07-14 16:20:47 +0700

    Add files via upload

---
+++ html/carousel.js
@@ -0,0 +1,75 @@
+document.write(`
+
+<div id="carouselExampleCaptions" class="carousel slide " data-bs-ride="carousel">
+
+    <div class="carousel-inner">
+      <div class="carousel-item active bg-1 ">
+        <div class="carousel-caption">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+
+        </div>
+      </div>
+      <div class="carousel-item bg-2">
+        <div class="carousel-caption">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item bg-3">
+        <div class="carousel-caption">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item bg-4">
+        <div class="carousel-caption">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item bg-5">
+        <div class="carousel-caption">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+
+
+
+    <div class="carousel-indicators">
+
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1">
+        <img src="213/Newfolder/gameTile__gt-Nv.png" class="d-block w-100 img-fluid slide-border">
+      </button>
+
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2">
+        <img src="213/Newfolder/gameTile__JSP4l.png" alt="" class="d-block w-100 img-fluid slide-border">
+      </button>
+
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3">
+        <img src="213/Newfolder/gameTile__1ksAF.png" alt="" class="d-block w-100 img-fluid slide-border">
+      </button>
+
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4">
+        <img src="213/Newfolder/gameTile__2gwVk.png" alt="" class="d-block w-100 img-fluid slide-border">
+      </button>
+
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 4">
+        <img src="213/Newfolder/gameTile__30EUT.png" alt="" class="d-block w-100 img-fluid slide-border">
+      </button>
+
+    </div>
+  </div>
+
+`)
